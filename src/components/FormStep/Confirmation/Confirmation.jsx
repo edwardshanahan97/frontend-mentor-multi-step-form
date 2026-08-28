2d12e58cd60d3b5428092cfcb986cc6cfd7500ea
Edward Shanahan <312564962+edwardshanahan97@users.noreply.github.com>
2026-08-28 14:34:58 +0100

    Add Confirmation component

--- src/components/FormStep/Confirmation/Confirmation.jsx
+++ src/components/FormStep/Confirmation/Confirmation.jsx
@@ -1,7 +1,17 @@
+import "./Confirmation.css";
+import IconTick from "../../../assets/images/icon-thank-you.svg";
 const Confirmation = () => {
   return (
     <div className="confirmation">
-      <h2>Confirmation</h2>
+      <img className="confirmation__img" src={IconTick} alt="Thank you icon" />
+
+      <h1 className="heading-1">Confirmation</h1>
+
+      <p className="lead">
+        Thanks for confirming your subscription! We hope you have fun using our
+        platform. If you ever need support, please feel free to email us at
+        support@loremgaming.com.
+      </p>
     </div>
   );
 };
